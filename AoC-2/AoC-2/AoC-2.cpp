72f2ffba8d657d2f12a49057867e617bf3458412
simonrosengren <simonrosengren123@gmail.com>
2017-12-21 22:01:58 +0100

    Init

---
+++ AoC-2/AoC-2/AoC-2.cpp
@@ -0,0 +1,14 @@
+// AoC-2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+using namespace std;
+
+int main()
+{
+	string input;
+
+    return 0;
+}
+
